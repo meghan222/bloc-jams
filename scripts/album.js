d48880b4aa737608f0a2437a132b51d5cdefbb97
meghan222 <meghan222@github.com>
2017-07-23 23:36:09 -0500

    Checkpoint 11 assignment done

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,20 @@ var albumPicasso = {
          { title: 'Wrong phone number', duration: '2:15'}
      ]
  };
+ var albumMeg = {
+     title: 'The Rose',
+     artist: 'FlowerShower',
+     label: 'Perfume',
+     year: '1922',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Old Gold?', duration: '4:01' },
+         { title: 'Tiny Tin', duration: '1:01' },
+         { title: 'Cat Hat', duration: '3:01'},
+         { title: 'Daisy?', duration: '3:14' },
+         { title: 'Pup', duration: '1:25'}
+     ]
+ };
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -39,13 +53,14 @@ var albumPicasso = {
 
      return template;
  };
- var setCurrentAlbum = function(album) {
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
       // #1
-      var albumTitle = document.getElementsByClassName('album-view-title')[0];
-      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-      var albumImage = document.getElementsByClassName('album-cover-art')[0];
-      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+    var setCurrentAlbum = function(album) {
 
       // #2
       albumTitle.firstChild.nodeValue = album.title;
@@ -64,4 +79,14 @@ var albumPicasso = {
 
   window.onload = function() {
       setCurrentAlbum(albumPicasso);
+    var albums = [albumPicasso, albumMarconi, albumMeg];
+    var index = 1;
+
+    albumImage.addEventListener("click", function(event){
+      setCurrentAlbum(albums[index]);
+      index++;
+      if (index == albums.length){
+        index = 0;
+      }
+    }
   };
